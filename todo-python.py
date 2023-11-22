b9c65019b3a6b2558d00a1301c8ad717e89878e9
Gustavout22 <gustavougartetorres@gmail.com>
2023-11-22 01:42:22 -0500

    add tasks option

--- todo-python.py
+++ todo-python.py
@@ -1,16 +1,72 @@
-tareas = [
-    "Hacer la compra",
-    "Estudiar para el examen",
-    "Hacer ejercicio",
-    "Llamar a mamá",
-    "estudiar python"
+# pylint: disable=missing-function-docstring
+tasks = [
+    {
+        "id": 1,
+        "todo_name": "Hacer la compra",
+        "completed": True
+    },
+    {
+        "id": 2,
+        "todo_name": "Estudiar para el examen",
+        "completed": True
+    },
+    {
+        "id": 3,
+        "todo_name": "Lavar los platos",
+        "completed": True
+    },
+    {
+        "id": 4,
+        "todo_name": "Estudiar python",
+        "completed": False
+    },
+]
+
+options = [
+    "tasks",
+    "add",
+    "delete",
+    "update",
+    "completed",
+    "uncompleted",
+    "exit"
 ]
 
 
+def list_tasks():
+    for task in tasks:
+        print(
+            f"{task['id']}. {task['todo_name']} { '|x|' if task['completed'] else '| |'}")
+
+
+def menu():
+    print("="*80)
+    print("#" * 25, " wELCOME TO MT TODO APP", "#" * 25)
+    print("="*80)
+    print(" | ".join(options))
+
+
 def iniciarApp():
-    print("#"*45)
-    print("######### BIENVENIDOS A MI TODO APP #########")
-    print("#"*45)
+    menu()
+    option = input("Choose option: ")
+
+    while option not in options:
+        print("=" * 80)
+        print(" | ".join(options))
+        option = input("Try again: ")
+
+    option = option.lower()
+
+    if option == "tasks":
+        list_tasks()
+    elif option == "add":
+        print("agregamos tarea")
+    elif option == "delete":
+        print("eliminamos tarea")
+    elif option == "update":
+        print("editamos tarea")
+    elif option == "completed":
+        print("tareas completadas")
 
 
 iniciarApp()
