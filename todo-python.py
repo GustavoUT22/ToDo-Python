8ae57e3452d8fead3b976b1228a5daaffc9bfb55
Gustavout22 <gustavougartetorres@gmail.com>
2023-11-21 23:46:12 -0500

    init todo

---
+++ todo-python.py
@@ -0,0 +1,16 @@
+tareas = [
+    "Hacer la compra",
+    "Estudiar para el examen",
+    "Hacer ejercicio",
+    "Llamar a mamá",
+    "estudiar python"
+]
+
+
+def iniciarApp():
+    print("#"*45)
+    print("######### BIENVENIDOS A MI TODO APP #########")
+    print("#"*45)
+
+
+iniciarApp()
